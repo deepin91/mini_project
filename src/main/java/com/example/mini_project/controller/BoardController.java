80aab5bbc69caa78a8fe1c80746edfea4ba3cfa0
deepin <deepin3809@gmail.com>
2024-09-27 21:22:54 +0900

    // add plan

--- src/main/java/com/example/mini_project/controller/BoardController.java
+++ src/main/java/com/example/mini_project/controller/BoardController.java
@@ -2,3 +2,10 @@ package com.example.mini_project.controller;
 
 public class BoardController {
 }
+
+//레포지토리 파일이랑 serviceImpl 파일 다 만든 다음
+//컨트롤러 제대로 작성 해서 이제 API 연동해야함
+// 크롬 확장 기능에서 테스트용으로 해보고!!!!!
+//나중에 리액트로 프론트 단 만들기
+//다 모든 플젝 다 완료되면 그 전에 했던 1,2차 JPA 등 모든 방법 동원해서 업그레이드 및 업데이트 하기
+//그런 후 포폴에 적용
